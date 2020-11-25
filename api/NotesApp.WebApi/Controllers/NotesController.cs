b375e66de7d306f5641996b7b7cf256d00e0827b
Pavel Kirilin <kirilin.pav@gmail.com>
2020-11-25 21:33:37 +0300

    Created WebApi project

---
+++ api/NotesApp.WebApi/Controllers/NotesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("notes")]
+    public class NotesController : ControllerBase
+    {
+        public NotesController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNotes()
+        {
+            return Ok();
+        }
+    }
+}
