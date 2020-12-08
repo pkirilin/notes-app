5b13f924225f240345f473f757f9a55b2d5aac7d
Pavel Kirilin <kirilin.pav@gmail.com>
2020-12-08 20:38:29 +0300

    Simplified LoginInput component test

--- client-app/src/features/auth/components/__tests__/LoginInput.test.tsx
+++ client-app/src/features/auth/components/__tests__/LoginInput.test.tsx
@@ -2,44 +2,35 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import LoginInput from '../LoginInput';
 import { render } from 'app/testing';
-import { AuthActionTypes } from 'features/auth/actions';
+import { AuthActions, AuthActionTypes } from 'features/auth/actions';
 
 describe('LoginInput component', () => {
-  test('should change login input', () => {
-    // Arrange
-    const changedLogin = 'new login';
-
-    // Act
-    const { getByPlaceholderText } = render(<LoginInput></LoginInput>);
-    const loginInput = getByPlaceholderText('Login');
-    fireEvent.change(loginInput, { target: { value: changedLogin } });
-
-    // Assert
-    expect(loginInput.getAttribute('value')).toBe(changedLogin);
-  });
-
-  test('should change password input', () => {
-    // Arrange
-    const changedPassword = 'new password';
-
-    // Act
-    const { getByPlaceholderText } = render(<LoginInput></LoginInput>);
-    const passwordInput = getByPlaceholderText('Password');
-    fireEvent.change(passwordInput, { target: { value: changedPassword } });
-
-    // Assert
-    expect(passwordInput.getAttribute('value')).toBe(changedPassword);
-  });
-
   test('should send login request on button click', () => {
+    // Arrange
+    const login = 'login';
+    const password = 'password';
+    const expectedActions: AuthActions[] = [
+      {
+        type: AuthActionTypes.LoginRequest,
+        payload: {
+          userName: login,
+          password,
+        },
+      },
+    ];
+
     // Act
-    const { getByText, store } = render(<LoginInput></LoginInput>);
+    const { getByText, getByPlaceholderText, store } = render(
+      <LoginInput></LoginInput>,
+    );
+    const loginInput = getByPlaceholderText('Login');
+    const passwordInput = getByPlaceholderText('Password');
     const signInButton = getByText('Sign in');
+    fireEvent.change(loginInput, { target: { value: login } });
+    fireEvent.change(passwordInput, { target: { value: password } });
     fireEvent.click(signInButton);
-    const actions = store.getActions();
 
     // Assert
-    expect(actions).toHaveLength(1);
-    expect(actions[0].type).toBe(AuthActionTypes.LoginRequest);
+    expect(store.getActions()).toEqual(expectedActions);
   });
 });
