394a5dacfdc4e907d62e0cba13e59e01b7544cdf
Pavel Kirilin <kirilin.pav@gmail.com>
2021-04-25 23:07:49 +0300

    Implemented fetch of notes list

--- client-app/src/features/notes/api/index.ts
+++ client-app/src/features/notes/api/index.ts
@@ -1,7 +1,16 @@
+import Cookies from 'js-cookie';
+import config from '../../../config';
+import { UserData } from '../../auth/models';
 import { NoteListItem } from '../models/NoteListItem';
 
 const getNotes = async (): Promise<NoteListItem[]> => {
-  return new Promise(resolve => resolve([]));
+  const user: UserData = Cookies.getJSON('auth');
+  const response = await fetch(`${config.apiUrl}/notes`, {
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  });
+  return response.json();
 };
 
 export default {
