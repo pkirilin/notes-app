3ab7c81ca7d5745c56303aead3e5fa1ee378e195
Pavel Kirilin <kirilin.pav@gmail.com>
2021-05-02 00:14:45 +0300

    Added App component tests

---
+++ client-app/src/App.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from './App';
+import { loginSuccess } from './features/auth/actions';
+import { UserData } from './features/auth/models/UserData';
+import { renderConnected } from './test-utils';
+
+describe('<App></App>', () => {
+  describe('when user is not authorized', () => {
+    test('should show login page', async () => {
+      const { findByText } = renderConnected(<App></App>);
+
+      expect(await findByText('Sign in')).toBeVisible();
+    });
+  });
+
+  describe('when user authorized', () => {
+    test('should show notes page', async () => {
+      const { findByText } = renderConnected(<App></App>, [
+        loginSuccess(createTestUser()),
+      ]);
+
+      expect(await findByText('Notes')).toBeVisible();
+    });
+  });
+});
+
+function createTestUser(): UserData {
+  return {
+    userId: 1,
+    userName: 'test',
+    token: '',
+    tokenExpirationInDays: 1,
+  };
+}
