bb4d1300ba6602e2b4777cd3349e4f245bc53807
Pavel Kirilin <kirilin.pav@gmail.com>
2021-05-08 22:58:54 +0300

    Added TextArea styles

--- client-app/src/features/__shared__/components/TextArea.tsx
+++ client-app/src/features/__shared__/components/TextArea.tsx
@@ -4,4 +4,26 @@ export type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 export const TextArea = styled.textarea`
   padding: ${props => props.theme.sizing.md};
+  background-color: transparent;
+  color: inherit;
+  border: ${props =>
+    `${props.theme.borders.default} solid ${props.theme.colors.backgroundActionDark}`};
+  border-radius: ${props => props.theme.sizing.sm};
+  outline: none;
+  transition: ${props => props.theme.transition.default};
+
+  &:focus {
+    box-shadow: ${props =>
+      `0 0 ${props.theme.sizing.md} ${props.theme.colors.backgroundActionDark}`};
+  }
+
+  &:disabled {
+    color: ${props => props.theme.text.default};
+    border: ${props =>
+      `${props.theme.borders.default} solid ${props.theme.colors.backgroundAction}`};
+  }
+
+  &::placeholder {
+    color: ${props => props.theme.text.hint};
+  }
 `;
--- client-app/src/features/__shared__/stories/TextArea.stories.tsx
+++ client-app/src/features/__shared__/stories/TextArea.stories.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { TextArea, TextAreaProps } from '../components';
+import styled from 'styled-components';
 
 export default {
   title: 'shared/TextArea',
   component: TextArea,
 } as Meta;
+
+const Wrapper = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+
+  & > :not(:first-child) {
+    margin-top: 16px;
+  }
+`;
+
+export const Examples: Story = () => (
+  <Wrapper>
+    <TextArea placeholder="Placeholder" cols={50}></TextArea>
+    <TextArea rows={5}>Some text</TextArea>
+    <TextArea disabled>Disabled</TextArea>
+  </Wrapper>
+);
 
 const Template: Story<TextAreaProps> = args => <TextArea {...args}></TextArea>;
 
