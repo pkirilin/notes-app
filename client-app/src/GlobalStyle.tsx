aeb91735dc912801b1f7c180d4a71bf5cabe9237
Pavel Kirilin <kirilin.pav@gmail.com>
2021-05-08 23:31:48 +0300

    Set global font size

--- client-app/src/GlobalStyle.tsx
+++ client-app/src/GlobalStyle.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     font-family: 'Roboto', sans-serif;
+    font-size: 14px;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
